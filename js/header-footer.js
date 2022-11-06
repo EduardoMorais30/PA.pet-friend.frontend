f77c2245a01d2f6f3a26375e12b14e546bfcb9cb
CGabrielGaspar <carlos.gaspar.1429414@sga.pucminas.br>
2022-11-06 14:43:52 -0300

    Descrição: Consolidação do modelo de Footer. Data: 06/11/2022 Autor: Gabriel Etapa: 3

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,3 +1,22 @@
+let coll = document.getElementsByClassName("collapsible");
+let i;
+let len = coll.length
+
+for (i = 0; i < len; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        console.log(content)
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+            content.style.borderBottom = null;
+
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
+
 function menuShow(){
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')){
@@ -6,3 +25,4 @@ function menuShow(){
         menuMobile.classList.add('open');
     }
 }
+
